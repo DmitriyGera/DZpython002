56577ac372177bb196a47ea69e2bad60e9329d25
DmitriyGera <dimmo12397@mail.ru>
2022-05-26 18:51:17 +0300

    Домашняя работа Герасимов Д А

---
+++ Zadacha004.py
@@ -0,0 +1,13 @@
+# Написать программу преобразования десятичного числа в двоичное
+
+n = int(input('Введите число: '))
+def convert_dec_to_bin(n):
+    bin_num = []
+    while n > 1:
+        bin_num.insert(0, n % 2)
+        n = n // 2
+    return bin_num
+
+print(convert_dec_to_bin(n))
+
+print(bin(n).replace('0b1',''))
